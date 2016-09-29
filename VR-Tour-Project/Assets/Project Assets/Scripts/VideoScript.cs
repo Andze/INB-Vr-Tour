be132e647172abe9881d29cf5e6ece83cfef2c17
Liam Wilson <liamryanwilson@hotmail.co.uk>
2016-09-30 00:05:13 +0100

    Lighting data...... Deleted?

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/VideoScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class VideoScript : MonoBehaviour
+{
+    private MovieTexture movie;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        Renderer r = GetComponent<Renderer>();
+        movie = (MovieTexture)r.material.mainTexture;
+        if (movie != null)
+        {
+            movie.Play();
+            movie.loop = true;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
