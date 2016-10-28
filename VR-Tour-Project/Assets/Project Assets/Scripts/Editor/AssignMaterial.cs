47581140b30e82676baf3412425e4b7f0b5cc5bd
Liam Wilson <liamryanwilson@hotmail.co.uk>
2016-10-28 14:50:21 +0100

    Lighting fixed finally!

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/Editor/AssignMaterial.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+public class AssignMaterial : ScriptableWizard
+{
+    public Material theMaterial;
+    String strHelp = "Select Game Objects";
+
+    void OnWizardUpdate()
+    {
+        helpString = strHelp;
+        isValid = (theMaterial != null);
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject[] objs = Selection.gameObjects;
+        for (int i = 0; i < objs.Length; i++)
+            objs[i].GetComponent<MeshRenderer>().material = theMaterial;
+    }
+
+    [MenuItem("Custom/Assign Material", false, 4)]
+    static void assignMaterial()
+    {
+        DisplayWizard("Assign Material", typeof(AssignMaterial), "Assign");
+    }
+}
