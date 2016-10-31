0379702961ece077cb6afed85cf790307c5479af
Liam Wilson <liamryanwilson@hotmail.co.uk>
2016-10-31 14:39:11 +0000

    Not many changes.

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/Editor/WS_Editor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(WaypointSystem))]
+public class WS_Editor : Editor
+{
+    WaypointSystem t;
+
+    void OnEnable()
+    {
+        t = (WaypointSystem)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        
+    }
+
+    void OnSceneGUI()
+    {
+        
+    }
+}
