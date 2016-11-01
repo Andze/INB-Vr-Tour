6523bb9775ef8ccb131966dd675268fd90654ab1
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-11-01 13:17:51 +0000

    Started work on Siemens Lounge

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/WhizDish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhizDish : MonoBehaviour {
+
+
+    int iStartTimeout;
+
+    // Use this for initialization
+    void Start () {
+
+        iStartTimeout = 25;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (iStartTimeout > 0)
+            iStartTimeout = iStartTimeout -1;
+    }
+
+}
