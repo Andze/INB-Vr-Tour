0028b110565b1d4af49f8096c38c6b2c46bfdef3
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-11-01 15:23:15 +0000

    Added Movement for camera using WizDish

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/Move_W.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_W : MonoBehaviour {
+
+
+    public float MovementSpeed = 5.0f;
+  
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * MovementSpeed;
+        }
+    
+    }
+}
