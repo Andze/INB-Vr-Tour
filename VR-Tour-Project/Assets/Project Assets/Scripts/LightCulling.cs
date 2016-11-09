eb07a87062db22b3e9681b1afe63fa9a754d3f07
Liam Wilson <liamryanwilson@hotmail.co.uk>
2016-11-09 14:14:29 +0000

    Lighting adjusted - major performance issues experienced.

---
+++ VR-Tour-Project/Assets/Project Assets/Scripts/LightCulling.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCulling : MonoBehaviour
+{
+    public float distance = 20.0f;
+
+    private Light[] lights;
+    void Start()
+    {
+        GameObject[] lightObj = GameObject.FindGameObjectsWithTag("Light");
+        lights = new Light[lightObj.Length];
+
+        for (int i = 0; i < lightObj.Length; i++)
+        {
+            lights[i] = lightObj[i].GetComponentInChildren<Light>();
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, lights[i].transform.position) > distance)
+            {
+                lights[i].enabled = false;
+            }
+            else lights[i].enabled = true;
+        }
+    }
+}
